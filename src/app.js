812b3e2189e417d63c1fcd58b9b08955bdd01346
mudale222 <yuziel@walla.co.il>
2020-04-04 21:20:22 +0300

    jsx,babel,store initial set and test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("work")
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import  configureStore  from './config/store'
+
+const store = configureStore()
+
+const jsx = (
+    <Provider store={store}>
+        <h1>testing</h1>
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
